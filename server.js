cd9c22d91450c176a1852be24184168497f22763
HamzaJirah <hamzajirah@gmail.com>
2018-02-16 15:12:02 +0100

    set up a route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(req, res){
+  
+})
 app.listen(3000, function(){
   console.log('server started on port 3000')
 });
