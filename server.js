d37efe8741a15fa043a61db9a41cfdf90717d678
HamzaJirah <hamzajirah@gmail.com>
2018-02-16 15:16:29 +0100

    created h1 tag

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-  
-})
 app.listen(3000, function(){
   console.log('server started on port 3000')
 });
+
+app.get('/', function(req, res){
+  res.send('Hello world')
+})
