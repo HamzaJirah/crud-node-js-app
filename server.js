5c8e6b0bc9a3c5accd8969fcb9da90f67b6fad2b
HamzaJirah <hamzajirah@gmail.com>
2018-02-17 07:12:41 +0100

    rendered the index page

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const path = required('path');
+const path = require('path');
 
 app.listen(3000, function(){
   console.log('server started on port 3000')
 });
 
-res.sendFile(path.resolve('index.html'));
-// app.get('/', (req, res) => {
-//   res.sendFile('/Users/Mc Anderson/Desktop/GENERAL/GIT-REPOS/CRUD ' + 'index.html')
-// });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
+});
