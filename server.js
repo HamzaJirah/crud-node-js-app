16cf7dc56043fb9d96089e7c280d888dedf744bd
HamzaJirah <hamzajirah@gmail.com>
2018-02-17 06:53:22 +0100

    required path   module

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = required('path');
 
 app.listen(3000, function(){
   console.log('server started on port 3000')
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'index.html')
-});
+res.sendFile(path.resolve('index.html'));
+// app.get('/', (req, res) => {
+//   res.sendFile('/Users/Mc Anderson/Desktop/GENERAL/GIT-REPOS/CRUD ' + 'index.html')
+// });
